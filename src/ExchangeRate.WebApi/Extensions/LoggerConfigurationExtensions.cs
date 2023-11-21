e9dd6567fbec1d127734d73975f03ca7fec5f90b
Filip Pindej <fpindej@users.noreply.github.com>
2023-11-21 23:21:25 +0100

    Create LoggerConfigurationExtensions

---
+++ src/ExchangeRate.WebApi/Extensions/LoggerConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+using Serilog;
+using Serilog.Events;
+
+namespace ExchangeRate.WebApi.Extensions;
+
+internal static class LoggerConfigurationExtensions
+{
+    public static void SetupLoggerConfiguration() => Log.Logger = new LoggerConfiguration()
+        .ConfigureBaseLogging()
+        .CreateLogger();
+
+    private static LoggerConfiguration ConfigureBaseLogging(
+        this LoggerConfiguration loggerConfiguration)
+    {
+        loggerConfiguration
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Console();
+
+        return loggerConfiguration;
+    }
+}
