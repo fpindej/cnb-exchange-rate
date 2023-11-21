ace167978f3fe8a6e14239adbf45eff30c1b9643
Filip Pindej <fpindej@users.noreply.github.com>
2023-11-22 00:04:09 +0100

    Use old Startup

--- src/ExchangeRate.WebApi/Program.cs
+++ src/ExchangeRate.WebApi/Program.cs
@@ -1,19 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using ExchangeRate.WebApi.Extensions;
+using Serilog;
+using LoggerConfigurationExtensions = ExchangeRate.WebApi.Extensions.LoggerConfigurationExtensions;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+namespace ExchangeRate.WebApi;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static int Main()
+    {
+        const string appName = "Exchange Rate API";
+        LoggerConfigurationExtensions.SetupLoggerConfiguration();
+        
+        try
+        {
+            Log.Information("Starting web host {AppName}", appName);
+            CreateHostBuilder().Build().Run();
+            Log.Information("Ending web host {AppName}", appName);
+            return 0;
+        }
+        catch (Exception e) when (e is not HostAbortedException)
+        {
+            Log.Fatal(e, "Host terminated unexpectedly {AppName}", appName);
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+    
+    private static IHostBuilder CreateHostBuilder()
+    {
+        return Host.CreateDefaultBuilder()
+            .UseSerilog(Log.Logger, true)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.Run();
